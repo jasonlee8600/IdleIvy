5c9dfe3b8e6b07670cb608dab031846346b1ed9d
gsleenhof <gabriel.sleenhof@yale.edu>
2023-02-24 13:02:01 -0500

    connecting contract in progress

--- idlyivy/components/Item.js
+++ idlyivy/components/Item.js
@@ -8,20 +8,19 @@ function Item({ title, desc, image }) {
 
 
             <div className='flex flex-row ml-[25px]'>
-              {/*<img className="h-[130px] w-[250px] mr-[60px] rounded-full outline outline-white outline-[7px]" src="../buttery.jpeg"></img>*/}
-              <Image src={image} width={130} height={100} className="rounded-full outline outline-white outline-[4px]"></Image>
+              <Image src={image} width={65} className="rounded-full outline outline-white outline-[4px]"></Image>
               
               <div className='flex flex-row w-full pl-4'>
                 
-                <div className='flex flex-row z-0 bg-gradient-to-b from-[#272727] to-[#797979] h-[130px] w-5/6 rounded-full outline outline-white outline-[4px]'>
+                <div className='flex flex-row z-0 bg-gradient-to-b from-[#272727] to-[#797979] h-[65px] w-5/6 rounded-full outline outline-white outline-[4px]'>
                   
-                    <button id="upgrade_buttery" type="button" className="self-start z-10 bg-[#202B64] h-[130px] w-[250px] rounded-full outline outline-white outline-[4px] text-4xl text-white">
+                    <button id="upgrade_buttery" type="button" className="self-start z-10 bg-[#202B64] h-[65px] w-1/4 rounded-full outline outline-white outline-[4px] text-xl text-white">
                         Upgrade
                     </button>
                   
-                    <div className='flex flex-col text-center'>
-                        <h3 className={`text-3xl text-white justify-center mb-[10px]`}>{title}</h3>
-                        <h4 className={`text-2xl text-white justify-center`}>{desc}</h4>
+                    <div className='flex flex-col justify-center w-4/5'>
+                        <h3 className={`text-2xl text-white text-center`}>{title}</h3>
+                        <h4 className={`text-lg text-white text-center`}>{desc}</h4>
                     </div>
                 </div>
               </div>
