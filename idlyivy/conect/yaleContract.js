148fbb5189dd3200fbf27a4a80565a2202b88865
gsleenhof <gabriel.sleenhof@yale.edu>
2023-03-31 09:52:08 -0400

    new contract

--- idlyivy/conect/yaleContract.js
+++ idlyivy/conect/yaleContract.js
@@ -1,7 +1,7 @@
 import { Contract } from '@ethersproject/contracts';
 import Yale from '../conect/Yale.json' 
 
-export const contractAddress = '0x926398b482685F327f7fCcFa9d6529D7bDcCF476';
+export const contractAddress = '0x8C256a9A6c874d91109D8bb6b3fFa52345cC9569';
 
 export const getContract = (library, account) => {
 	const signer = library.getSigner(account).connectUnchecked();
