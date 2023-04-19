cd213a05ddf83e6a310c9534211df8cbbede3593
Jason Lee <70392664+jasonlee8600@users.noreply.github.com>
2023-04-19 11:35:51 -0400

    README update

---
+++ idlyivy/__tests__/frontend_tests/SideNav.test.js
@@ -0,0 +1,14 @@
+import SideNav from '../../components/SideNav'
+import {render, screen, fireEvent} from '@testing-library/react'
+
+test('Should return to homepage when clicking IdleIvy', () => {
+    render(<SideNav />)
+    const IdleIvy_btn = screen.getByTestId('return-home')
+    fireEvent.click(IdleIvy_btn)
+    expect(window.location.href).toEqual('http://localhost/')
+});
+
+test('Check title is IdleIvy', () => {
+    render(<SideNav />)
+    expect(screen.getByTestId('IdleIvy').innerHTML).toEqual('IdleIvy')
+});
