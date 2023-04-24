dae5567892ede885fe2a8fe29e10eb6f0b2e61c0
Jason Lee <70392664+jasonlee8600@users.noreply.github.com>
2023-04-24 16:01:02 -0400

    pending design

--- idlyivy/components/Pending.js
+++ idlyivy/components/Pending.js
@@ -7,7 +7,7 @@ function Pending({ pending }) {
     {pending ?
         <div className="h-screen z-30 fixed inset-y-[0px] inset-x-0 bg-black  bg-opacity-50  flex justify-center items-center">
         <div className="w-full flex flex-col justify-center items-center gap-[10px] mt-4">
-            <div className='flex w-1/2 h-1/2 bg-white items-center justify-center py-[20px] rounded-full outline outline-black outline-[4px]'>
+            <div className='flex w-1/2 h-1/2 bg-white items-center justify-center py-[20px] outline outline-black outline-[4px]'>
             <p className='text-xl text-black text-center'>Transaction Pending...<br/>
             Please wait for Metamask transaction to complete.<br/>
             Refresh the page if not loaded after 1 minute.</p>
